f77bfa9af9b9ed4030d394019aaf658e6e01a1b1
Alikhan Khakimov <akhakimov@dept-info.crosemont.quebec>
2024-05-20 03:54:01 -0400

    Menu presque fini

---
+++ NeonBlitz3D/Assets/Scenes/Scripts/EventMenuPrincipal.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class EventMenuPrincipal : MonoBehaviour
+{
+    public void Jouer() { 
+    
+    }
+
+    public void Quitter() {
+        Application.Quit();
+    }
+}
